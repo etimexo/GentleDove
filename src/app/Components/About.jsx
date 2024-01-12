43e50255ae3a3848d44ac6953e70bbbb894dfffc
Elijah Obisesan <elijahobisesan01@gmail.com>
2024-01-12 20:23:04 +0100

    Style about.jsx

--- src/app/Components/About.jsx
+++ src/app/Components/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Typist from 'react-typist';
 // import {gentleDove} from '../assets/gentledove.jpg'
 import gentleDove from "../assets/gentledove.jpg";
 import "./about.css";
@@ -19,6 +20,31 @@ export default function () {
             About
         </button><br />
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. <br />A dolorum nulla nesciunt repudiandae corrupti, facilis, vitae voluptates optio, quia eaque dolor vero fugit <br /> numquam cum error exercitationem repellat rem. Consequatur?
+        {/* <Typed 
+            strings = {[
+              "I'm a full stack developer",
+              "I'm a cool person",
+              "just testing...",
+            ]}
+            typeSpeed={110}
+            backSpeed={80}
+            loop
+          />
+        <p> */}gfefr
+        <Typist>
+          Hey
+        </Typist>
+          {/* <Typed 
+            strings = {[
+              "I'm a full stack developer",
+              "I'm a cool person",
+              "just testing...",
+            ]}
+            typeSpeed={110}
+            backSpeed={80}
+            loop
+          /> */}
+        {/* </p> */}
       </div>
     </div>
   );
